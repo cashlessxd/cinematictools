e40773606b91ddae1f6b24c1aa7fa4c335e3a46c
cashlessxd <mnoel8393@gmail.com>
2023-06-17 13:54:01 +0200

    Added Logic to save clip in user/documents/cinematictools/clips/

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
+import os
+import shutil
 import time
 import vgamepad
 from initializer import cameraSettings as cam, controllerInput as ctr, videoSettings as vid
 from mss import mss
 
 import moviepy.video.io.ImageSequenceClip
-from PIL import Image, ImageFile
 
 def run():
     amountFrames = vid.videoLength * vid.framesPerSecond
     gamepad = vgamepad.VX360Gamepad()
+
+    clipPath = os.environ['USERPROFILE'] + '/Documents/Cinematictools/clips'
+
+    os.makedirs('frames')
+
+    if not os.path.exists(clipPath):
+        os.makedirs(clipPath)
 
     with mss() as sct:
         for frame in range(amountFrames):
@@ -17,7 +25,9 @@ def run():
                 focusCamera(gamepad)
             createFrame(sct, frame)
 
-    createVideo(amountFrames)
+    createVideo(amountFrames, clipPath)
+
+    shutil.rmtree('frames')
 
 def moveCameraOneFrame(gamepad):
     gamepad.left_joystick_float(x_value_float=ctr.leftStickX, y_value_float=ctr.leftStickY)
@@ -36,7 +46,7 @@ def focusCamera(gamepad):
     gamepad.press_button(button=vgamepad.XUSB_BUTTON.XUSB_GAMEPAD_X)
     gamepad.update()
 
-    time.sleep(0.01) #not very pretty
+    time.sleep(0.01) #TODO: make this bit prettier
 
     gamepad.release_button(button=vgamepad.XUSB_BUTTON.XUSB_GAMEPAD_X)
     gamepad.update()
@@ -44,13 +54,18 @@ def focusCamera(gamepad):
 def createFrame(sct, frame):
     sct.shot(output='frames/frame' + str(frame) + '.png')
 
-def createVideo(amountFrames):
+def createVideo(amountFrames, clipPath):
     frames = []
+    clipNumber = 1
+
+    for file in os.listdir(clipPath):
+        print(file)
+        clipNumber += 1
 
     for frameNumber in range(amountFrames):
         frames.append('frames/frame' + str(frameNumber) + '.png')
 
     clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(frames, fps=vid.framesPerSecond)
-    clip.write_videofile('clip.mp4', bitrate='1000000k')
+    clip.write_videofile(clipPath + '/clip' + str(clipNumber) + '.mp4', bitrate='1000000k')
 
 run()
